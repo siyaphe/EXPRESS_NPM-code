0f092821f6da13a20051a17c63da73d179b1d31e
siyaphe <siyapdje@yahoo.fr>
2024-04-12 22:04:31 -0400

    Final Commit

--- index.js
+++ index.js
@@ -10,23 +10,6 @@
 
 // --> (?) THE BIG 3 ------- runningTheServer
 // 1. use require statements to import all of the needed depend/libs for the app.
-
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {  
-//     res.statusCode = 200;  
-//     res.setHeader('Content-Type', 'text/html');  
-//     res.write('<h1 style="color: red">Hello World!</h1>'); 
-//     res.write('<p>I wonder what else we can send...</p>');  res.end();});
-
-
-// server.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-
 const express = require("express");
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || "3000";
